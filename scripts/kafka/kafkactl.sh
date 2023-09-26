ad43ddfa02a92035c9e218a17a07e941966720a4
Ernesto Salgado Suárez <ernesto.salgado.suarez@gmail.com>
2023-09-26 13:42:16 +0200

    Fix network name

--- scripts/kafka/kafkactl.sh
+++ scripts/kafka/kafkactl.sh
@@ -6,4 +6,4 @@
 # containerized infra just execute "make start-infra".
 # -----------------------------------------------------------------------------
 docker run --platform linux/amd64 --rm --env BROKERS="domain-kafka:9092" \
-    --network kafka-infra_compose_network -v ./kafkactl_config.yaml:/etc/kafkactl/config.yml deviceinsight/kafkactl "$@"
+    --network f4allgo-network -v ./kafkactl_config.yaml:/etc/kafkactl/config.yml deviceinsight/kafkactl "$@"
