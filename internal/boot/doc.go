bf1aca127808faae28e1bd76e50156fd283c25c1
Ernesto Salgado Suárez <ernesto.salgado.suarez@gmail.com>
2023-09-26 19:23:21 +0200

    Update boot package docs

--- internal/boot/doc.go
+++ internal/boot/doc.go
@@ -1,4 +1,6 @@
-// Package boot provides utility functions to initialize some application
-// dependencies like loggers, the database connections and other required
-// initializations.
+// Package boot provides utility functions to initialize application dependencies
+// at boot time (like loggers, database connections or metrics), normally from the
+// main function. It also serves the adapter layer as a central package for accessing
+// some useful initialized dependencies like application configuration (obtained from
+// the environment) or the metrics scope. Don't import this package from the core layer.
 package boot
