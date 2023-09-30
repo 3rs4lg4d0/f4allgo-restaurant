3c5cd25dbc3ccb08c6cbb01cf015436082ae54fd
Ernesto Salgado Suárez <ernesto.salgado.suarez@gmail.com>
2023-09-30 17:55:40 +0200

    Automate grafana dashboard import when creating the infra

--- scripts/infra/start-local-infra.sh
+++ scripts/infra/start-local-infra.sh
@@ -8,8 +8,31 @@ if ! docker network inspect $NETWORK_NAME >/dev/null 2>&1; then
 fi
 
 cd "$BASE_DIR/deployments/kafka" && docker-compose up -d
-cd "$BASE_DIR/deployments/metrics" && docker-compose up -d
+
+cd "$BASE_DIR/deployments/metrics" && docker-compose up -d && \
+    docker run -it --rm --network $NETWORK_NAME --volume $BASE_DIR/assets/grafana:/root ubuntu bash -c ' \
+         echo -e "Installing packages..." && apt update > /dev/null 2>&1 && apt install -y curl jq netcat > /dev/null 2>&1 && \
+         while ! nc -zv prometheus 9090; do echo "Waiting for Prometheus to be ready..."; sleep 1; done && \
+         while ! nc -zv grafana 3000; do echo "Waiting for Grafana to be ready..."; sleep 1; done && \
+         response=$(curl -s -X POST http://grafana:3000/api/datasources -H "Content-Type: application/json" --data "{
+            \"name\": \"Prometheus for f4allgo-restaurant\",
+            \"type\": \"prometheus\",
+            \"access\": \"proxy\",
+            \"url\": \"http://prometheus:9090\"
+         }") && \
+         echo -e "Prometheus datasource created ✅" && \
+         datasource_uid=$(echo "$response" | jq -r ".datasource.uid") && \
+         dashboard_content=$(cat /root/f4allgo-restaurant-dashboard.json) && \
+         payload=$(echo { \
+             \"dashboard\": $dashboard_content, \
+             \"overwrite\":true, \
+             \"inputs\":[\{\"name\":\"DS_PROMETHEUS\",\"type\":\"datasource\",\"pluginId\":\"prometheus\",\"value\":\"$datasource_uid\"\}], \
+             \"folderUid\":\"\"}) && \
+         curl -s -o /dev/null -X POST http://grafana:3000/api/dashboards/import -H "Content-Type: application/json" --data "$payload" && \
+         echo -e "Grafana dashboard imported ✅"
+    '
+
 cd "$BASE_DIR/deployments/postgres" && docker-compose up -d && \
     docker-compose exec postgres bash -c \
-        "while ! pg_isready; do echo 'Waiting for postgres to be ready...'; sleep 1; done" && cd ../..
+        "while ! pg_isready; do echo 'Waiting for postgres to be ready...'; sleep 1; done"
 cd -
