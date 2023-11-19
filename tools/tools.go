d57a072bc6be28f2ebd72358ea7f73f73e2d34c9
Ernesto Salgado Suárez <ernesto.salgado.suarez@gmail.com>
2023-11-19 14:39:22 +0100

    chore: add grpc tools to the tools package

--- tools/tools.go
+++ tools/tools.go
@@ -7,4 +7,6 @@ import (
 	_ "github.com/golang-migrate/migrate/v4/cmd/migrate"
 	_ "github.com/golangci/golangci-lint/cmd/golangci-lint"
 	_ "github.com/vektra/mockery/v2/cmd"
+	_ "google.golang.org/grpc/cmd/protoc-gen-go-grpc"
+	_ "google.golang.org/protobuf/cmd/protoc-gen-go"
 )
